6ea6c73f9c4cf9a1baa78938dc30e17ee7f898bc
Xanh <xuananh7029@gmail.com>
2024-02-27 10:18:16 +0700

    new

--- tuan 6/bt.js
+++ tuan 6/bt.js
@@ -4,14 +4,16 @@ var man = [
         name:"Quần Shorts Nam chạy bộ Ultra",
         code: "TC102",
         price: "250.000",
+        oldPrice: "350.000",
         image: "https://product.hstatic.net/1000271846/product/pants-beige-01_7caa0ecd9ff040e3a231d89f7e4ea5db_master.jpg"
 
-    },
+    }, 
     {
         id:2,
         name:"Quần Shorts Black",
         code: "TC103",
         price: "350.000",
+        oldPrice: "450.000",
         image: "https://media.coolmate.me/cdn-cgi/image/quality=80,format=auto/uploads/March2023/s6.917_72.jpg"
 
     },
@@ -20,6 +22,7 @@ var man = [
         name:"Quần Shorts BEIGE",
         code: "TC104",
         price: "150.000",
+        oldPrice: "400.000",
         image: "https://product.hstatic.net/1000271846/product/pants-beige-01_fcff1444268b47ffacc171b2748dbe5a_master.jpg"
     },
 ];
@@ -29,6 +32,7 @@ var women = [
         name:"Váy Ngắn Nữ",
         code: "EC201",
         price: "450.000",
+        oldPrice: "550.000",
         image: "https://img.fantaskycdn.com/7aa7a791569b5d7707cee30a83b39bb9_750x.jpeg"
     },
     {
@@ -36,6 +40,7 @@ var women = [
         name:"Chân Váy Nữ Xếp Ly",
         code: "EC202",
         price: "190.000",
+        oldPrice: "550.000",
         image: "https://canifa.com/img/1000/1500/resize/6/k/6ks23w008-sk010-1-thumb.webp"
     },
     {
@@ -43,6 +48,7 @@ var women = [
         name:"Váy Tay Bồng Cúp Ngực",
         code: "EC203",
         price: "400.000",
+        oldPrice: "600.000",
         image: "https://bizweb.dktcdn.net/100/438/408/products/vay-dam-nu-van6146-tra-1-yodyvn.jpg"
     },
 ];
@@ -51,17 +57,18 @@ function listProducts(){
     {
         var demo = '<div class= "col-3">';
         demo += '<div class= "card" style="width: 18rem;">';
-        demo += '<img src="' +man[i].image +'" class = "card-img-top" style="height:400px;">';
-        demo += '<div class= "card-body">';
+        demo += '<img src="' +man[i].image +'" class = "card-img-top" style="height:400px;">'; 
+        demo += '<div class= "card-body">'; 
         demo += '<h5 class="card-title">'+man[i].name+'</h5>';
-        demo += '<p class="card-text">'+man[i].price+'</p>';
+        demo += '<p class="card-text">'+man[i].price+'</p>'; 
+        demo += '<p class="card-text"><span class="old-price">Giá cũ: ' + man[i].oldPrice + '</span></p>';
         demo += '<a href="#" class="btn btn-primary" onclick="oder()">Đặt mua</a>';
         demo += '</div>';
         demo += '</div>';
         demo += '</div>';
         console.log(demo);
-        document.getElementById("men").innerHTML += demo;
-    }
+        document.getElementById("men").innerHTML += demo; 
+    }   
     for(let i = 0; i <= women.length-1; i++){
         var demo = '<div class = "col-3">';   
         demo += '<div class="card" style="width: 18rem; ">';
@@ -69,6 +76,7 @@ function listProducts(){
         demo += '<div class="card-body">';
         demo += '<h5 class="card-title">'+women[i].name+'</h5>';
         demo += '<p class="card-text">'+women[i].price+'</p>';
+        demo += '<p class="card-text"><span class="old-price">Giá cũ: ' + women[i].oldPrice + '</span></p>';
         demo += '<a href="#" class="btn btn-primary" onclick="oder()">Đặt mua</a>';
         demo += '</div>';
         demo += '</div>';
